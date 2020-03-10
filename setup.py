1f2bd29a9e364019a684367f0d42b64a83463ea5
Joerg Walter <jwdev@posteo.de>
2020-03-10 15:56:56 +0100

    setup.py: raise version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 		name = 'lazysnapshotter',
-		version = '0.1',
+		version = '0.3',
 		description = 'Frontend for btrfs-subvolume based backups.',
 		classifiers = [
 			'Development Status :: 3 - Alpha',
